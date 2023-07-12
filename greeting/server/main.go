48fc44dc7c8217aa2f91bd46a06c714e00c0556b
twalluxio <tongyu.wu@alluxio.com>
2023-07-12 14:19:37 +0800

    debugged server display

--- greeting/server/main.go
+++ greeting/server/main.go
@@ -14,19 +14,19 @@ type server struct {
 const port = ":50051"
 
 func (s *server) SayHello(ctx context.Context, in *greeter.HelloRequest) (*greeter.HelloReply, error) {
-	log.Printf("Received: #{in.GetName()}")
+	log.Println("Received:", in.GetName())
 	return &greeter.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Println("Failed to listen at port " + port)
+		log.Println("Failed to listen at localhost" + port)
 		return
 	}
 	s := grpc.NewServer()
 	greeter.RegisterGreeterServer(s, &server{})
-	log.Println("Server listening at port " + port)
+	log.Println("Server listening at localhost" + port)
 	if err := s.Serve(lis); err != nil {
 		log.Println("Failed to serve ", err)
 		return
