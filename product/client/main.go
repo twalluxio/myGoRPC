288c09d58b4848f5ccc0bc17e214a73ceccc4107
twalluxio <tongyu.wu@alluxio.com>
2023-07-12 12:13:31 +0800

    create a product client

---
+++ product/client/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"myGoRPC/product"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Println("Not connected", err)
+		return
+	}
+	defer conn.Close()
+
+	client := product.NewProductInfoClient(conn)
+	ctx := context.Background()
+
+	id := AddProduct(ctx, client)
+	GetProduct(ctx, client, id)
+}
+
+func AddProduct(ctx context.Context, client product.ProductInfoClient) (id string) {
+	aMac := &product.Product{Name: "Mac Book Pro 2019", Description: "From Apple Inc."}
+	productId, err := client.AddProduct(ctx, aMac)
+	if err != nil {
+		log.Println("add product fail.", err)
+		return
+	}
+	log.Println("add product success, id = ", productId.Value)
+	return productId.Value
+}
+
+func GetProduct(ctx context.Context, client product.ProductInfoClient, id string) {
+	p, err := client.GetProduct(ctx, &product.ProductId{Value: id})
+	if err != nil {
+		log.Println("get product err.", err)
+		return
+	}
+	log.Printf("get prodcut success : %+v\n", p)
+}
